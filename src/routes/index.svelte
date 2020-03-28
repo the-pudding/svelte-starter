0ded0e9f00a180edab8b145293e42adcd6b2da7d
russellgoldenberg <russellgoldenberg@gmail.com>
2020-03-28 11:16:14 -0400

    reset css

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,8 +2,7 @@
   import App from "../components/App.svelte";
 </script>
 
-<style global src="../../static/assets/global.css">
-
-</style>
+<style global src="../../static/assets/reset.css"></style>
+<style global src="../../static/assets/global.css"></style>
 
 <App />
