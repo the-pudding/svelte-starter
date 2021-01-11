577490c25d2423d8218d4a0a20bb5d6217e97ae6
russellgoldenberg <russellgoldenberg@gmail.com>
2021-01-11 09:48:08 -0500

    more params for io

--- src/actions/inView.js
+++ src/actions/inView.js
@@ -6,20 +6,39 @@
  * 	on:enter={() => console.log("enter")}
  * 	on:exit={() => console.log("exit")}
  * >
+ * 
+ * optional params { root, top, bottom }
+ * top and bottom are numbers
+ * use:inView={ bottom: 100 } // 100 pixels from bottom of viewport
  */
 
-export default function inView(node) {
-	const options = { root: null };
+export default function inView(node, params = {}) {
+	let observer;
+
 	const handleIntersect = (e) => {
 		const v = e[0].isIntersecting ? "enter" : "exit";
 		node.dispatchEvent(new CustomEvent(v));
 	};
-	const observer = new IntersectionObserver(handleIntersect, options);;
-	observer.observe(node);
+	
+	const setObserver = ({ root, top, bottom }) => {
+		const marginTop = top ? top * -1 : 0;
+		const marginBottom = bottom ? bottom * -1 : 0;
+		const rootMargin = `${marginTop}px 0px ${marginBottom}px 0px`;
+		const options = { root, rootMargin };
+		if (observer) observer.disconnect();
+		observer = new IntersectionObserver(handleIntersect, options);;
+		observer.observe(node);
+	}
+
+	setObserver(params);
 
 	return {
+		update(params) {
+			setObserver(params);
+		},
+
 		destroy() {
-			observer.disconnect();
+			if (observer) observer.disconnect();
 		}
 	};
 }
