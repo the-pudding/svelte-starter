0dc766fae0f6495093de0c76eba69109a1c8816d
russellgoldenberg <russellgoldenberg@gmail.com>
2021-02-19 07:24:49 -0500

    update deploy

--- src/components/demo/Demo.svelte
+++ src/components/demo/Demo.svelte
@@ -66,34 +66,34 @@
         </Svg>
       </LayerCake>
     </figure>
+  </div>
 
-    <div>
-      <h1>Heading 1</h1>
-      <h2>Heading 2</h2>
-      <h3>Heading 3</h3>
-      <h4>Heading 4</h4>
-      <h5>Heading 5</h5>
-      <h6>Heading 6</h6>
-      <p>
-        This is a paragraph with a <strong>strong</strong> tag and an
-        <em>em</em> tag. And here is a <mark>mark</mark> tag. Oh and a
-        <a href="#demo">link</a> tag.
-      </p>
-    </div>
+  <div>
+    <h1>Heading 1</h1>
+    <h2>Heading 2</h2>
+    <h3>Heading 3</h3>
+    <h4>Heading 4</h4>
+    <h5>Heading 5</h5>
+    <h6>Heading 6</h6>
+    <p>
+      This is a paragraph with a <strong>strong</strong> tag and an
+      <em>em</em> tag. And here is a <mark>mark</mark> tag. Oh and a
+      <a href="#demo">link</a> tag.
+    </p>
+  </div>
 
-    <div>
-      <p>Some Elements:</p>
-      <p><button>Button</button></p>
-      <p>
-        <select>
-          <option>Select</option>
-          <option>Thing 1</option>
-          <option>Thing 2</option>
-        </select>
-      </p>
-      <p><input placeholder="input" /></p>
-      <p><input type="range" /></p>
-    </div>
+  <div>
+    <p>Some Elements:</p>
+    <p><button>Button</button></p>
+    <p>
+      <select>
+        <option>Select</option>
+        <option>Thing 1</option>
+        <option>Thing 2</option>
+      </select>
+    </p>
+    <p><input placeholder="input" /></p>
+    <p><input type="range" /></p>
   </div>
 </section>
 
