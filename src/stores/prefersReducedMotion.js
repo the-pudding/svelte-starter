a5b4b1a56935732310d579699309bc525827e721
russellgoldenberg <russellgoldenberg@gmail.com>
2021-03-03 08:42:48 -0500

    fix reduced motion store

--- src/stores/prefersReducedMotion.js
+++ src/stores/prefersReducedMotion.js
@@ -2,16 +2,16 @@ import { readable } from "svelte/store";
 
 export default readable(false, (set) => {
 	const query = "(prefers-reduced-motion: no-preference)";
-	const mediaQueryList = window ? window.matchMedia(query) : {};
+	const mediaQueryList = typeof window !== "undefined" ? window.matchMedia(query) : {};
 
 	const onChange = () => set(!mediaQueryList.matches);
 	
-	if (window) {
+	if (typeof window !== "undefined") {
 		mediaQueryList.addEventListener("change", onChange);
 		onChange();
 	}
 	
-	return () => mediaQueryList.removeEventListener('change', onChange);
+	return () => {
+		if (typeof window !== "undefined") mediaQueryList.removeEventListener('change', onChange);
+	}
 });
-
-
