0d5a7959a2dce58b340bc97eada192ec656b72c4
russellgoldenberg <russellgoldenberg@gmail.com>
2021-05-21 11:54:33 -0400

    meta

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -1,32 +1,38 @@
+<script>
+  export let title = "Title TK";
+  export let description = "Description TK";
+  export let url = "https://pudding.cool";
+</script>
+
 <svelte:head>
-  <title>Svelte Starter</title>
+  <title>{title}</title>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <meta name="description" content="" />
-  <meta name="author" content="" />
+  <meta name="description" content={description} />
+  <meta name="author" content="The Pudding" />
   <meta name="news_keywords" content="" />
 
-  <meta property="og:title" content="" />
-  <meta property="og:site_name" content="" />
-  <meta property="og:url" content="" />
-  <meta property="og:description" content="" />
+  <meta property="og:title" content={title} />
+  <meta property="og:site_name" content="The Pudding" />
+  <meta property="og:url" content={url} />
+  <meta property="og:description" content={description} />
   <meta property="og:type" content="article" />
   <meta property="og:locale" content="en_US" />
 
-  <meta property="og:image" content="" />
+  <meta property="og:image" content="{url}/assets/social-facebook.jpg" />
   <meta property="og:image:type" content="image/jpeg" />
   <meta property="og:image:width" content="1200" />
   <meta property="og:image:height" content="600" />
 
   <meta name="twitter:card" content="summary_large_image" />
-  <meta name="twitter:site" content="" />
-  <meta name="twitter:creator" content="" />
-  <meta name="twitter:title" content="" />
-  <meta name="twitter:description" content="" />
-  <meta name="twitter:image:src" content="" />
+  <meta name="twitter:site" content="https://pudding.cool" />
+  <meta name="twitter:creator" content="@puddingviz" />
+  <meta name="twitter:title" content={title} />
+  <meta name="twitter:description" content={description} />
+  <meta name="twitter:image:src" content="{url}/assets/social-twitter.jpg" />
 
   <meta name="robots" content="max-image-preview:large" />
 
-  <link rel="canonical" href="" />
+  <link rel="canonical" href="{url}/" />
 </svelte:head>
