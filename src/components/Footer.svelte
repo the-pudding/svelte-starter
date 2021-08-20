c6bcab480fea504c138c12c608bab8a875786ebc
russellgoldenberg <russellgoldenberg@gmail.com>
2021-08-20 12:43:36 -0400

    fix footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -30,8 +30,8 @@
     const topic = story ? story.topic : "culture";
     const others = data.filter((d) => !localURL.includes(d.url));
 
-    const diff = data.filter((d) => d.topic !== topic);
-    const same = data.filter((d) => d.topic === topic);
+    const diff = others.filter((d) => d.topic !== topic);
+    const same = others.filter((d) => d.topic === topic);
 
     stories.push(...diff.slice(0, 2));
     stories.push(same[0]);
