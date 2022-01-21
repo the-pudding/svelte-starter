194509a73ff23d2b80c12f4ff65610e31db26774
russellgoldenberg <russellgoldenberg@gmail.com>
2022-01-21 09:19:59 -0500

    work in progress warning

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,7 +1,9 @@
 <script>
   import Demo from "$components/helpers/Demo.svelte";
+  import WIP from "$components/helpers/WIP.svelte";
   import Footer from "$components/Footer.svelte";
 </script>
 
+<WIP />
 <Demo />
 <!-- <Footer /> -->
---
+++ src/components/helpers/WIP.svelte
@@ -0,0 +1,17 @@
+<div>🚧 This project is a work in progress. Please do not share the URL. 🚧</div>
+
+<style>
+  div {
+    position: fixed;
+    z-index: 999999999;
+    top: 0;
+    left: 0;
+    width: 100%;
+    text-align: center;
+    padding: 1em;
+    background: pink;
+    color: black;
+    font-family: monospace;
+    font-size: 16px;
+  }
+</style>
