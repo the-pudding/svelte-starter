3fec08981e1ad18462b698b184dcee07fd97ecdc
russellgoldenberg <russellgoldenberg@gmail.com>
2022-04-06 09:17:40 -0400

    prehookup copy doc and metadata

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,7 +1,10 @@
 <script>
+  import { getContext } from "svelte";
   import Demo from "$components/demo/Demo.svelte";
   import WIP from "$components/helpers/WIP.svelte";
   // import Footer from "$components/Footer.svelte";
+
+  const copy = getContext("copy");
 </script>
 
 <WIP />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,21 @@
 <script>
+  import { setContext } from "svelte";
   import { browser } from "$app/env";
   import Meta from "$components/Meta.svelte";
   import App from "$components/App.svelte";
+  import copy from "$data/doc.json";
   if (browser) console.log("svelte-starter version", __VERSION__);
+
+  const preloadFont = [
+    "https://pudding.cool/assets/fonts/tiempos/TiemposTextWeb-Regular.woff2",
+    "https://pudding.cool/assets/fonts/tiempos/TiemposTextWeb-Bold.woff2",
+    "https://pudding.cool/assets/fonts/national/National2Web-Regular.woff2",
+    "https://pudding.cool/assets/fonts/national/National2Web-Bold.woff2"
+  ];
+
+  const { title, description, url, keywords } = copy;
+  setContext("copy", copy);
 </script>
 
-<Meta />
+<Meta {title} {description} {url} {preloadFont} {keywords} />
 <App />
