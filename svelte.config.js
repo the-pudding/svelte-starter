8919a2c52dc43386496a27e7afe05d5f3eae14cc
russellgoldenberg <russellgoldenberg@gmail.com>
2022-07-21 09:24:55 -0400

    update github pages

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,9 @@ import adapterStatic from "@sveltejs/adapter-static";
 import sveltePreprocess from "svelte-preprocess";
 import autoprefixer from "autoprefixer";
 
+
 const { subdirectory } = JSON.parse(readFileSync("package.json", "utf8"));
-const dev = process.env.NODE_ENV === "development";
+const dev = process.env.NODE_ENV !== "production";
 const dir = subdirectory || "";
 const prefix = dir.startsWith("/") ? "" : "/";
 const base = dev || !dir ? "" : `${prefix}${dir}`;
