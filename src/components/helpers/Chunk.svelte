9cfa1b2f73cae41695d12783e3fa57bee7569e44
russellgoldenberg <russellgoldenberg@gmail.com>
2022-07-22 18:53:36 -0400

    add chunk

---
+++ src/components/helpers/Chunk.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let text = "";
+	export let max = text.length;
+	export let space = true;
+	export let tag = "span";
+
+	$: regex = new RegExp(`[\\s\\S]{1,${max}}(?!\\S)`, "g");
+	$: chunks = text
+		.replace(regex, "$&\n")
+		.split("\n")
+		.map((d) => d.trim())
+		.filter((d) => d)
+		.map((d, i) => `${i > 0 && space ? "&nbsp;" : ""}${d}`);
+</script>
+
+{#each chunks as chunk}
+	<svelte:element this={tag} class="chunk">{@html chunk}</svelte:element>
+{/each}
+
+<style>
+	.chunk {
+		display: inline-block;
+	}
+</style>
