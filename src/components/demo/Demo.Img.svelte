458b21e51ae9c42565b8b760fec5c26157e26bfb
russellsamora <russellsamora@gmail.com>
2023-06-21 11:15:55 -0400

    test img

--- src/components/demo/Demo.Img.svelte
+++ src/components/demo/Demo.Img.svelte
@@ -1,10 +1,21 @@
 <section id="demo-image">
 	<h2>Image</h2>
+	<p>img tag</p>
 	<img src="assets/demo/test.jpg" alt="cat" />
+	<p>background image</p>
+	<div />
 </section>
 
 <style>
 	img {
-		width: 120px;
+		width: 128px;
+	}
+
+	div {
+		width: 128px;
+		height: 128px;
+		background: url("assets/demo/test.jpg");
+		background-size: cover;
+		background-position: center center;
 	}
 </style>
