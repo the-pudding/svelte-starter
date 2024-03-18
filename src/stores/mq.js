40246a86e757c0da9e2b111b90dd9f143067d43b
Russell Samora <russellsamora@gmail.com>
2024-03-18 13:12:45 -0400

    add mq

--- src/stores/mq.js
+++ src/stores/mq.js
@@ -9,8 +9,9 @@ const queries = {
 	"60rem": "(min-width: 60rem)",
 	"70rem": "(min-width: 70rem)",
 	"80rem": "(min-width: 80rem)",
-	"reducedMotion": "(prefers-reduced-motion: reduce)",
-	"desktop": "(hover: hover) and (pointer: fine)"
+	reducedMotion: "(prefers-reduced-motion: reduce)",
+	colorSchemeDark: "(prefers-color-scheme: dark)",
+	desktop: "(hover: hover) and (pointer: fine)"
 };
 
 function calculateMedia(mqls) {
