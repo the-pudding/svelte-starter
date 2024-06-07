b348977b3aaa6f39a1fc4787e7a006ec0159677f
Russell Samora <russellsamora@gmail.com>
2024-06-07 06:05:08 -0400

    fixes #47

--- src/stores/previous.js
+++ src/stores/previous.js
@@ -1,9 +1,9 @@
 import { derived } from "svelte/store";
 
-export default previous = (store) => {
+export default (store) => {
 	let p = null;
 
-	return derived(store, current => {
+	return derived(store, (current) => {
 		const previous = p;
 		p = current;
 		return previous;
