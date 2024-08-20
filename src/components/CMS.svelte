2c65d93831ec7e15533a5b48cf86574fbfa89924
Russell Samora <russellsamora@gmail.com>
2024-08-20 14:50:16 -0400

    cms

---
+++ src/components/CMS.svelte
@@ -0,0 +1,28 @@
+<script>
+	// an object of components that map to section names (e.g., { "Hero": Hero }) where Hero is a Svelte component
+	export let components = {};
+	// an array of objects that contain a section name and content (either an array of object)
+	export let body = [];
+</script>
+
+{#each body as { section, content }}
+	{@const id = section.toLowerCase()}
+	{@const component = components[section]}
+	<section {id}>
+		{#if component}
+			<svelte:component this={component} {...content}></svelte:component>
+		{:else}
+			{#each content as { type, value }}
+				{#if components[type]}
+					<svelte:component this={component} {...value}></svelte:component>
+				{:else if type === "text"}
+					<p>{@html value}</p>
+				{:else}
+					<svelte:element this={type} {...value.attributes}
+						>{#if typeof value === "string"}{@html value}{/if}</svelte:element
+					>
+				{/if}
+			{/each}
+		{/if}
+	</section>
+{/each}
