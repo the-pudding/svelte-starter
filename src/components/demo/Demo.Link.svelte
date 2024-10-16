21766046641e0eaae8e157690e6724bf595c8aa3
Russell Samora <russellsamora@gmail.com>
2024-10-16 13:56:08 -0400

    demo links relative

--- src/components/demo/Demo.Link.svelte
+++ src/components/demo/Demo.Link.svelte
@@ -1,5 +1,5 @@
 <section id="demo-link">
 	<h2>Link</h2>
-	<p><a href="demo/elements">Default element styles demo</a></p>
-	<p><a href="demo/fonts">Pudding-hosted font previews</a></p>
+	<p><a href="elements">Default element styles demo</a></p>
+	<p><a href="fonts">Pudding-hosted font previews</a></p>
 </section>
