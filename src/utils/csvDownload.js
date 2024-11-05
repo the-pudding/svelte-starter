1468bbdef0fdc0704024a312a2c224da7b17800a
David Mora <davidnmora@gmail.com>
2024-11-04 19:38:45 -0800

    Update csvDownload.js to include import of csvFormat from d3

--- src/utils/csvDownload.js
+++ src/utils/csvDownload.js
@@ -1,4 +1,6 @@
+import { csvFormat } from "d3";
+
 export default function csvDownload(data) {
-	const content = `data:text/csv;charset=utf-8,${d3.csvFormat(data)}`;
+	const content = `data:text/csv;charset=utf-8,${csvFormat(data)}`;
 	return encodeURI(content);
 }
