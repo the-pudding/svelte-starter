04cf372d9634a26341548f8deba58eb253557081
Russell Samora <russellsamora@gmail.com>
2026-02-24 13:22:30 -0500

    tip component

---
+++ src/components/helpers/Tip.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Button from "$components/ui/Button.svelte";
+	import DollarSign from "@lucide/svelte/icons/dollar-sign";
+	let { text = "Tip", href = "#" } = $props();
+</script>
+
+<Button {href} target="_blank" rel="noreferrer">
+	{text} <span><DollarSign color="currentColor" /></span>
+</Button>
+
+<style>
+	span {
+		display: inline-block;
+		width: 1.25rem;
+		height: 1.25rem;
+		margin-left: 0.25rem;
+	}
+</style>
